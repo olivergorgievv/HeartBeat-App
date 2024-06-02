666bd753b39a9e12c15a8a6d21b9ed005b1b649e
Oliver <oliver.gorgiev11@gmail.com>
2024-06-02 10:40:59 +0200

    adding featured in section

--- src/components/Featured in/FeaturedIn.jsx
+++ src/components/Featured in/FeaturedIn.jsx
@@ -1,5 +1,20 @@
-function Cta() {
-  return <div>Cta</div>;
+import "./FeaturedIn.css";
+
+function FeaturedIn() {
+  return (
+    <section className="section-featured">
+      <div className="container">
+        <h2 className="heading-featured-in">As Featured In</h2>
+        <div className="logos">
+          <img src="https://omnifood.dev/img/logos/techcrunch.png" />
+          <img src="https://omnifood.dev/img/logos/business-insider.png" />
+          <img src="https://omnifood.dev/img/logos/the-new-york-times.png" />
+          <img src="https://omnifood.dev/img/logos/forbes.png" />
+          <img src="https://omnifood.dev/img/logos/usa-today.png" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Cta;
+export default FeaturedIn;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Navigation from "../components/Navigation/Navigation";
 import Hero from "../components/Hero/Hero";
 import HowItWorks from "../components/HowItWorks/HowItWorks";
+import FeaturedIn from "../components/Featured in/FeaturedIn";
 
 function HomePage() {
   return (
     <>
       <Navigation />
       <Hero />
+      <FeaturedIn />
       <HowItWorks />
     </>
   );
