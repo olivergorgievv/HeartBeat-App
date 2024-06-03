fe685bf07f0ea7f018ccd28c64910fdd48f080c5
Oliver <oliver.gorgiev11@gmail.com>
2024-06-03 17:40:37 +0200

    form block progress

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -5,7 +5,45 @@ function CallToAction() {
     <section className="section-cta">
       <div className="container">
         <div className="cta">
-          <div className="cta-text-box">CTA</div>
+          <div className="cta-text-box">
+            <h2 className="heading-secondary">
+              Get your first meals for free!
+            </h2>
+            <p className="cta-text">
+              Healthy, tasty and hassle-free meals are waiting for you. Start
+              eating well today. You can cancel or pause anytime. And the first
+              meal is on us!
+            </p>
+            <form className="cta-form" action="#">
+              <div>
+                <label htmlFor="name">Full Name</label>
+                <input id="name" type="text" placeholder="John Smith" />
+              </div>
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="me@example.com"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="origin">Where did you hear from us?</label>
+                <select name="" id="origin">
+                  <option value="friends">Please choose one option:</option>
+                  <option value="friends">Friends and Family</option>
+                  <option value="youtube">Youtube Video</option>
+                  <option value="podcast">Podcast</option>
+                  <option value="facebook">Facebook ad</option>
+                  <option value="others">Others</option>
+                </select>
+              </div>
+              <button className="btn" type="submit">
+                Sign up now
+              </button>
+            </form>
+          </div>
           <div className="cta-img-box">Image</div>
         </div>
       </div>
