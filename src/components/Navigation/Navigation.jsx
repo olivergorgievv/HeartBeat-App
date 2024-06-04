6606e571a4584187ba3344777d6dd50f436b8f7e
Oliver <oliver.gorgiev11@gmail.com>
2024-06-04 15:42:20 +0200

    Adding Mobile navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,50 +1,57 @@
+import { useState } from "react";
 import logo from "../../assets/logo.svg";
 import "./Navigation.css";
 
 function Cta() {
+  const [isOpen, setIsOpen] = useState("");
+
   return (
     <main className="section-header">
-      <header className="header">
+      <header className={`header ${isOpen}`}>
         <a href="#">
-          <img src={logo} className="logo" />
+          <img src={logo} className="logo" alt="Logo" />
         </a>
         <nav className="main-nav">
           <ul className="main-nav-list">
             <li>
-              {" "}
               <a className="main-nav-link" href="#">
                 How it Works
               </a>
             </li>
             <li>
-              {" "}
               <a className="main-nav-link" href="#">
                 Meals
               </a>
             </li>
             <li>
-              {" "}
               <a className="main-nav-link" href="#">
                 Testimonials
               </a>
             </li>
             <li>
-              {" "}
               <a className="main-nav-link" href="#">
                 Pricing
               </a>
             </li>
             <li>
-              {" "}
               <a className="main-nav-link nav-cta" href="#">
                 Try for free
               </a>
             </li>
           </ul>
         </nav>
-        <button className="btn-mobile-nav">
-          <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
-          <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
+        <button
+          onClick={() => {
+            setIsOpen(isOpen === "" ? "nav-open" : "");
+          }}
+          className="btn-mobile-nav"
+        >
+          <span className="icon-container">
+            <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
+            {isOpen === "nav-open" && (
+              <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
+            )}
+          </span>
         </button>
       </header>
     </main>
